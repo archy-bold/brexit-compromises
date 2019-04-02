fd5c2cc9ceca1dd19a66f78d2b2dd80aaceca865
Simon Archer <simon@archybold.com>
2019-04-02 14:49:03 +0100

    Creating a votes topic and producing some votes to it.

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create the topics
+# kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic raw-votes
+
+# Produce the votes to the topic
+votes=( \
+
+`# Indicative Votes 2` \
+1108904 `# Common Market 2.0` \
+1108905 `# Customs Union` \
+1108906 `# Public Vote` \
+1108907 `# Parliamentary Supremacy ` \
+)
+
+for i in "${votes[@]}";
+do
+    curl http://lda.data.parliament.uk/commonsdivisions/id/$i.json | jq -c . | kafka-console-producer --broker-list localhost:9092 --topic raw-votes;
+done
