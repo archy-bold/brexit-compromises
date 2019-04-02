cf2e2fbd9802d546792db04fa9250d545c4ecb1b
Simon Archer <simon@archybold.com>
2019-04-02 18:54:09 +0100

    Added all votes.

--- produce.sh
+++ produce.sh
@@ -2,6 +2,20 @@
 
 # Produce the votes to the topic
 votes=( \
+1041567 `# Meaningful Vote 1`
+1086876 `# Meaningful Vote 2`
+
+`# Indicative Votes 1` \
+1105521 `# No Deal ` \
+1105524 `# Common Market 2.0` \
+1105526 `# EFTA & EEA` \
+1105527 `# Customs Union` \
+1105529 `# Labour's Plan` \
+1105530 `# Revoke` \
+1105532 `# Public Vote` \
+1105533 `# Contingent preferential arrangements` \
+
+1107737 `# Meaningful Vote 3`
 
 `# Indicative Votes 2` \
 1108904 `# Common Market 2.0` \
