1db2338cfd0ba2a350a97bcd52887f2380c56649
Simon Archer <simon@archybold.com>
2019-04-02 17:24:07 +0100

    Registering minister and party schemas.

--- init.sh
+++ init.sh
@@ -6,6 +6,22 @@ dir="${0%/*}"
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic raw-votes
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 4 --topic individual-votes
 
+# Register the schemas
+declare -A schemas=( \
+["MINISTERS_STREAM"]="$dir/schema/ministers.avsc" \
+["PARTIES_STREAM"]="$dir/schema/parties.avsc" \
+)
+
+for topic in "${!schemas[@]}";
+do
+    jq --arg text "$(<${schemas[$topic]})" '.schema=$text' <<< "{}" | \
+        curl -d @- -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+        http://localhost:8081/subjects/$topic-value/versions
+    curl -X POST -i -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+        --data '{"schema": "{\"type\": \"string\"}"}' \
+        http://localhost:8081/subjects/$topic-key/versions
+done
+
 # Create the streams
 ksql < $dir/ksql/streams.ksql
 
