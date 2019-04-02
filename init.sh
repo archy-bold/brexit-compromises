c51f360f7154fc5d34ce9c50f24d53ec7473b7aa
Simon Archer <simon@archybold.com>
2019-04-02 16:59:50 +0100

    Produce the individual votes to another topic using jq.

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ dir="${0%/*}"
 
 # Create the topics
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic raw-votes
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic individual-votes
 
 # Create the streams
 ksql < $dir/ksql/streams.ksql
@@ -20,5 +21,12 @@ votes=( \
 
 for i in "${votes[@]}";
 do
-    curl http://lda.data.parliament.uk/commonsdivisions/id/$i.json | jq -c . | kafka-console-producer --broker-list localhost:9092 --topic raw-votes;
+    # Publish the whole result, but it's probably not useful
+    curl http://lda.data.parliament.uk/commonsdivisions/id/$i.json | \
+        jq -c . | kafka-console-producer --broker-list localhost:9092 --topic raw-votes;
+    # Publish the individual votes
+    curl http://lda.data.parliament.uk/commonsdivisions/id/$i.json | \
+        jq -c '.result.primaryTopic.vote[]' | \
+        sed "s/^/$i|/" | \
+        kafka-console-producer --broker-list localhost:9092 --topic individual-votes --property "parse.key=true" --property "key.separator=|"
 done
