b88a488d652204939aafe39423d617dc77bcee84
Simon Archer <simon@archybold.com>
2019-04-10 17:38:17 +0100

    Add the build.

---
+++ build/precache-manifest.7fc3276f932b4b58862bd9e2100d51eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0474ccc7c9a451ffb30b",
+    "url": "/static/js/main.a233ee50.chunk.js"
+  },
+  {
+    "revision": "371900319b997d90d46a",
+    "url": "/static/js/2.7c3a9a1f.chunk.js"
+  },
+  {
+    "revision": "0474ccc7c9a451ffb30b",
+    "url": "/static/css/main.8a0f8d26.chunk.css"
+  },
+  {
+    "revision": "371900319b997d90d46a",
+    "url": "/static/css/2.effbcc75.chunk.css"
+  },
+  {
+    "revision": "f6c9339aff9b25bb75bee304202fd156",
+    "url": "/index.html"
+  }
+];
